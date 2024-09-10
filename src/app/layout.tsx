4f6ba513d6c8b9f4d6b15ea7c103a4d895ef3d04
Samir Shaikh <samirshaikh.co.in@gmail.com>
2024-09-10 23:12:15 +0530

    add commit from two

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* remove the above comment */}
+      {/* this comment from Two */}
       <body>
         <Appbar />
         <div className="relative overflow-hidden">{children}</div>
